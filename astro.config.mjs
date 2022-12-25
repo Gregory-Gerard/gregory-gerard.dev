949df6846d486976619f46c9c82400ab5fc2363c
Grégory <contact@gregory-gerard.dev>
2022-12-25 21:50:02 +0100

    chore: add some plugins for generated content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,10 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'vitesse-dark',
+    },
+  },
 });
