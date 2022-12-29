3052402162755e85cb3b43f67ad522da7ff25516
Grégory <contact@gregory-gerard.dev>
2022-12-29 21:41:58 +0100

    fix: responsive Breadcrumb.tsx

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -6,15 +6,15 @@ export default function Breadcrumb(props: { pages: NonEmptyArray<{ name: string;
   const lastPage = props.pages.pop();
 
   return (
-    <div className="flex items-center gap-8 w-full pb-8 border-b border-zinc-800">
-      <a href="/" title="Page d'accueil">
+    <div className="flex flex-col sm:flex-row sm:items-center gap-8 w-full pb-8 border-b border-zinc-800">
+      <a href="/" title="Page d'accueil" className="shrink-0">
         <Avatar className="h-16 w-16" />
       </a>
 
       <nav className="flex px-5 py-3 rounded-2xl bg-zinc-800/50 shadow-outline" aria-label="Breadcrumb">
-        <ol className="inline-flex items-center space-x-1 md:space-x-3">
-          <li className="inline-flex items-center">
-            <a href="/" className="inline-flex items-center text-sm font-medium text-zinc-400 hover:text-zinc-50">
+        <ol className="flex items-center space-x-1 md:space-x-3 min-w-0">
+          <li className="flex items-center">
+            <a href="/" className="flex items-center text-sm font-medium text-zinc-400 hover:text-zinc-50">
               <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
               </svg>
@@ -25,18 +25,7 @@ export default function Breadcrumb(props: { pages: NonEmptyArray<{ name: string;
           {props.pages.map((item) => (
             <li key={item.name}>
               <div className="flex items-center">
-                <svg
-                  className="w-6 h-6 text-zinc-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                <Arrow />
 
                 <a href={item.url} className="ml-1 text-sm font-medium md:ml-2 text-zinc-400 hover:text-zinc-50">
                   {item.name}
@@ -46,26 +35,33 @@ export default function Breadcrumb(props: { pages: NonEmptyArray<{ name: string;
           ))}
 
           {lastPage && (
-            <li aria-current="page">
+            <li aria-current="page" className="min-w-0">
               <div className="flex items-center">
-                <svg
-                  className="w-6 h-6 text-zinc-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="ml-1 text-sm font-medium md:ml-2 text-zinc-300">{lastPage.name}</span>
+                <Arrow />
+
+                <span className="ml-1 text-sm font-medium md:ml-2 text-zinc-300 truncate">{lastPage.name}</span>
               </div>
             </li>
           )}
         </ol>
       </nav>
     </div>
+  );
+}
+
+function Arrow() {
+  return (
+    <svg
+      className="w-6 h-6 text-zinc-400 shrink-0"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
   );
 }
