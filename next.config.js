4f39df3a90d79c00485188d74a8ce816ba598e9d
Grégory <contact@gregory-gerard.dev>
2024-05-20 02:42:59 +0200

    chore: upgrade packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   modularizeImports: {
     'react-feather': {
       transform: 'react-feather/dist/icons/{{ lowerCase member }}',
